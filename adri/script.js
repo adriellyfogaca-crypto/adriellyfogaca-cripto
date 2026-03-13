b29bf5bd3824c59b5d7074c34ca50a55eba470ab
adriellyfogaca-crypto <adrielly.fogaca@escola.pr.gov.br>
2026-03-13 11:00:09 -0300

    Update script.js

--- adri/script.js
+++ adri/script.js
@@ -3,4 +3,25 @@ function salvar() {
     let idade = document.getElementById("idade").value;
     console.log("Nome:", nome);
     console.log("Idade:", idade);
-}
+
+}express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'escola4'
+});
+
+app.post("/salvar",(req, res) => {
+let nome = req.body.nome;
+let idade = req.body.idade;
+
+let sql = "INSERT INTO alunos(nome,idade) VALUES (?, ?);"
+})
